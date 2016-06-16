473198d13e6319294814bbb49a6a358d0c130397
nikolai0045 <tanner.ball@campusoutreach.org>
2016-06-16 11:34:05 -0400

    added PhoneNumber and EmailAddress to admin

--- src/supporttracker/admin.py
+++ src/supporttracker/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Person, UserProfile, SupportRelationship, ContactRelationship, Letter, Call, Meeting, ThankYou, VoiceMail, Referral, Reminder, Note, Message
+from .models import Person, UserProfile, SupportRelationship, ContactRelationship, Letter, Call, Meeting, ThankYou, VoiceMail, Referral, Reminder, Note, Message, PhoneNumber, EmailAddress
 
 class UserProfileInline(admin.StackedInline):
 	model = UserProfile
@@ -13,6 +13,8 @@ class UserAdmin(BaseUserAdmin):
 	inlines = (UserProfileInline,)
 	
 admin.site.unregister(User)
+admin.site.register(PhoneNumber)
+admin.site.register(EmailAddress)
 admin.site.register(User,UserAdmin)
 admin.site.register(Person)
 admin.site.register(SupportRelationship)
