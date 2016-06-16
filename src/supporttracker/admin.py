15cfab64639e8411aad9e9343e749239e0377800
nikolai0045 <tanner.ball@campusoutreach.org>
2016-06-16 15:41:34 -0400

    made it so first row of csv upload is treated as a label

--- src/supporttracker/admin.py
+++ src/supporttracker/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Person, UserProfile, SupportRelationship, ContactRelationship, Letter, Call, Meeting, ThankYou, VoiceMail, Referral, Reminder, Note, Message, PhoneNumber, EmailAddress
+#from .models import Person, UserProfile, SupportRelationship, ContactRelationship, Letter, Call, Meeting, ThankYou, VoiceMail, Referral, Reminder, Note, Message, PhoneNumber, EmailAddress
 
 class UserProfileInline(admin.StackedInline):
 	model = UserProfile
