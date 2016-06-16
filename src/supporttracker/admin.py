44a2bf2bbeececcb1731a308458d6efddff24c03
nikolai0045 <tanner.ball@campusoutreach.org>
2016-06-16 15:42:25 -0400

    told csv reader to treat first row as a header

--- src/supporttracker/admin.py
+++ src/supporttracker/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
 #from .models import Person, UserProfile, SupportRelationship, ContactRelationship, Letter, Call, Meeting, ThankYou, VoiceMail, Referral, Reminder, Note, Message, PhoneNumber, EmailAddress
-
+from .models import UserProfile
 class UserProfileInline(admin.StackedInline):
 	model = UserProfile
 	can_delete = False
@@ -13,19 +13,19 @@ class UserAdmin(BaseUserAdmin):
 	inlines = (UserProfileInline,)
 	
 admin.site.unregister(User)
-admin.site.register(PhoneNumber)
-admin.site.register(EmailAddress)
-admin.site.register(User,UserAdmin)
-admin.site.register(Person)
-admin.site.register(SupportRelationship)
-admin.site.register(ContactRelationship)
-admin.site.register(Letter)
-admin.site.register(Call)
-admin.site.register(Meeting)
-admin.site.register(ThankYou)
-admin.site.register(VoiceMail)
-admin.site.register(Referral)
-admin.site.register(Reminder)
-admin.site.register(Note)
-admin.site.register(Message)
+# admin.site.register(PhoneNumber)
+# admin.site.register(EmailAddress)
+# admin.site.register(User,UserAdmin)
+# admin.site.register(Person)
+# admin.site.register(SupportRelationship)
+# admin.site.register(ContactRelationship)
+# admin.site.register(Letter)
+# admin.site.register(Call)
+# admin.site.register(Meeting)
+# admin.site.register(ThankYou)
+# admin.site.register(VoiceMail)
+# admin.site.register(Referral)
+# admin.site.register(Reminder)
+# admin.site.register(Note)
+# admin.site.register(Message)
 
